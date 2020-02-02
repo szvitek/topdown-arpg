3ba9b7ef685da5d1fc4300c571208cd41d9d443e
Gyula Szvitek <gy.szvitek@gmail.com>
2020-02-02 00:53:53 +0000

    chore: refactor into classes

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,25 @@
+import Phaser from "phaser";
+import logoImg from "../assets/logo.png";
+
+export default class GameScene extends Phaser.Scene {
+  constructor(key) {
+    super(key);
+  }
+
+  preload() {
+    this.load.image("logo", logoImg);
+  }
+
+  create() {
+    const logo = this.add.image(400, 150, "logo");
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1
+    });
+  }
+}
