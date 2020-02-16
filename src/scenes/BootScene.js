f46738d23c70928b1eeed25e9aa5d0dd46757015
Gyula Szvitek <gy.szvitek@gmail.com>
2020-02-16 00:52:05 +0000

    fix: imports, disable debug mode

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,4 +1,11 @@
 import Phaser from "phaser";
+import level1 from "../../src/assets/tilemaps/level1.json";
+import level2 from "../../src/assets/tilemaps/level2.json";
+import RPGpack from "../../src/assets/images/RPGpack_sheet.png";
+import charecters from "../../src/assets/images/roguelikeChar_transparent.png";
+import portal from "../../src/assets/images/raft.png";
+import coin from "../../src/assets/images/coin.png";
+import bullet from "../../src/assets/images/ball.png";
 
 export default class BootScene extends Phaser.Scene {
   constructor(key) {
@@ -11,37 +18,29 @@ export default class BootScene extends Phaser.Scene {
       2: "level2"
     };
     // load the tilemap
-    this.load.tilemapTiledJSON("level1", "src/assets/tilemaps/level1.json");
-    this.load.tilemapTiledJSON("level2", "src/assets/tilemaps/level2.json");
+    this.load.tilemapTiledJSON("level1", level1);
+    this.load.tilemapTiledJSON("level2", level2);
 
     // laad spritesheets
-    this.load.spritesheet(
-      "RPGpack_sheet",
-      "src/assets/images/RPGpack_sheet.png",
-      {
-        frameWidth: 64,
-        frameHeight: 64
-      }
-    );
+    this.load.spritesheet("RPGpack_sheet", RPGpack, {
+      frameWidth: 64,
+      frameHeight: 64
+    });
 
     // laad in character spritesheet
-    this.load.spritesheet(
-      "characters",
-      "src/assets/images/roguelikeChar_transparent.png",
-      {
-        frameWidth: 17,
-        frameHeight: 17
-      }
-    );
+    this.load.spritesheet("characters", charecters, {
+      frameWidth: 17,
+      frameHeight: 17
+    });
 
     // laad in protal sprite
-    this.load.image("portal", "src/assets/images/raft.png");
+    this.load.image("portal", portal);
 
     // coin
-    this.load.image("coin", "src/assets/images/coin.png");
+    this.load.image("coin", coin);
 
     // bullet
-    this.load.image("bullet", "src/assets/images/ball.png");
+    this.load.image("bullet", bullet);
   }
 
   create() {
